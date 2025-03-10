b0ed6007e6d8cc2a1b999a14f748d8f2e4d15c8b
jessiphilip <jessicameenuga@gmail.com>
2025-03-10 20:56:34 +0530

    Delete models directory

--- models/account.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const accountSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-  balance: { type: Number, default: 0 }
-});
-
-const Account = mongoose.model("accounts", accountSchema);
-export default Account;
